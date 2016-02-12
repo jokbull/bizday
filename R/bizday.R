07f9a7ad5843b044978d73c64e8c076475808b1f
jokbull <jokbullcn@gmail.com>
2016-02-12 07:42:24 +0000

    Update

--- R/bizday.R
+++ R/bizday.R
@@ -1,5 +1,5 @@
 .onLoad <- function(libname, pkgname){
-  data(holidayCN)
+  data(holidayCN,package = pkgname)
   that = Calendar(name="CN", holidays = holidayCN)
   op <- options()
   if (!"calendar" %in% names(op))  options(calendar=that)
